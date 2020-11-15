12172334f0e2b0ac409f4b2f7ca551c0aebced81
josepebel <josepe.beldad@gmail.com>
2020-11-15 22:51:03 +0100

    commit1

--- portfolio/sec.py
+++ portfolio/sec.py
@@ -2,12 +2,12 @@
 from pathlib import Path
 import os
 
-'''
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 '''
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+'''
 DEBUG = True
 SECRET_KEY = '40t7x(($30uev!#*+!2@#m0u^m#2tf5)-ng2vhx$3&*(f^f!wo'
 DATABASES = {
