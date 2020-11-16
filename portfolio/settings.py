aeebf862d7b9d31ccf2e91e375d2eb9406dd73f4
josepebel <josepe.beldad@gmail.com>
2020-11-16 10:51:48 +0100

    commitfinal

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,27 +9,24 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+from pathlib import Path
 import os
-from .sec import DEBUG, SECRET_KEY, DATABASES, BASE_DIR
-'''
-import environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-# reading .env file
-environ.Env.read_env()
-'''
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY 
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value') 
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG 
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
@@ -81,7 +78,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 
 # Password validation
